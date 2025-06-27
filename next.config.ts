d1f075e2e492ed849a8085c56241a59edf694931
Akshit Jain <toakshit2004@gmail.com>
2025-06-27 09:41:28 +0530

    Enable image imports via Webpack in Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["flagsapi.com"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|webp)$/i,
+      type: 'asset/resource',
+    });
+    return config;
   },
 };
 
