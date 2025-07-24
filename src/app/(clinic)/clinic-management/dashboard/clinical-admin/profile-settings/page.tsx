549e64c08d1eb4334a2bd8173aa099f74a11810d
sgarvit7 <ssgarvit7@gmail.com>
2025-07-24 10:49:05 +0530

    changes

--- src/app/(clinic)/clinic-management/dashboard/clinical-admin/profile-settings/page.tsx
+++ src/app/(clinic)/clinic-management/dashboard/clinical-admin/profile-settings/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 // import { motion } from "framer-motion";
 // import Image from "next/image";
 // import { Award, Power, Edit, Briefcase, GraduationCap, Lock, Bell, Play } from "lucide-react";
